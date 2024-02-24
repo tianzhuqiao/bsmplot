5d816e2631535f0fdaf15cf159b17851810006c8
Tianzhu Qiao <tq@feiyilin.com>
2024-02-24 10:33:05 -0800

    ver 0.1.1

--- bsmplot/version.py
+++ bsmplot/version.py
@@ -2,8 +2,8 @@ PROJECT_NAME     = 'bsmplot'
 
 VER_MAJOR        = 0
 VER_MINOR        = 1
-VER_RELEASE      = 0
+VER_RELEASE      = 1
 
-VER_FLAGS        = "a1"
+VER_FLAGS        = ""
 
 __version__ = f'{VER_MAJOR}.{VER_MINOR}.{VER_RELEASE}{VER_FLAGS}'
