e1bb32c819e85fa4c45980475806d204987e6eb1
Tianzhu Qiao <tq@feiyilin.com>
2024-12-10 13:03:52 -0800

    Open text file in editor and ignore certain files.

--- bsmplot/bsm/editor.py
+++ bsmplot/bsm/editor.py
@@ -4,13 +4,17 @@ from bsmutility.editor import Editor
 class PyEditor(Editor):
 
     @classmethod
-    def initialized(cls): 
+    def initialized(cls):
         super().initialized()
         dp.send(signal='shell.run',
                 command='from bsmplot.bsm.editor import PyEditor as Editor',
                 prompt=False,
                 verbose=False,
                 history=False)
+
+    @classmethod
+    def ignore_file_ext(cls):
+        return ['.csv', '.ulg', '.mat', '.vcd']
 
 def bsm_initialize(frame, **kwargs):
     """initialize the model"""
